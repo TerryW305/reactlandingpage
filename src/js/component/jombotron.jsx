7f22f55d85d07ee3124e47c766218b017e5dc75c
TerryW305 <terrywelch30625@gmail.com>
2022-07-20 16:33:30 +0000

    landingpage

---
+++ src/js/component/jombotron.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+
+export const Jombotron = () => {
+	return (
+		<div class="card w-75 mx-auto">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+</div>
+			
+	
+	);
+};
+
+
